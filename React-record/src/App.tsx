188f2f22f6408412407b863fd513839baa0104f8
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-03-13 20:32:36 +0900

    わかんないとこの解説

--- React-record/src/App.tsx
+++ React-record/src/App.tsx
@@ -3,35 +3,37 @@ import React, { useState, useRef } from "react";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const AudioRecorder: React.FC = () => {
-  const [recording, setRecording] = useState<boolean>(false);
-  const [audioURL, setAudioURL] = useState<string | null>(null);
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
+  const [recording, setRecording] = useState<boolean>(false); //録音状態
+  const [audioURL, setAudioURL] = useState<string | null>(null);//録音ファイル格納url
+  const mediaRecorder = useRef<MediaRecorder | null>(null);//メディアレコーダ
   const audioChunks = useRef<Blob[]>([]);
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
+  } = useSpeechRecognition();//音声テキスト化API
 
   if (!browserSupportsSpeechRecognition) {
-    return <span>ブラウザが音声認識未対応です</span>;
+    return <span>ブラウザが音声認識未対応です</span>;//Firefox等APIが利用できない場合文字列のみを表示
   }
 
+  //録音開始ボタン押した時の挙動
   const startRecording = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder.current = new MediaRecorder(stream);
-    mediaRecorder.current.ondataavailable = (event) => {
-      audioChunks.current.push(event.data);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });//マイクの使用許可、マイクの音声入力を取得
+    mediaRecorder.current = new MediaRecorder(stream);//録音を管理するオブジェクトのインスタンスを作成
+    mediaRecorder.current.ondataavailable = (event) => {//録音データが利用可能になるたび
+      audioChunks.current.push(event.data);//録音データを配列 audioChunks に蓄積
     };
+    //録音が終了したときの挙動の定義
     mediaRecorder.current.onstop = () => {
-      const audioBlob = new Blob(audioChunks.current, { type: "audio/wav" });
-      const url = URL.createObjectURL(audioBlob);
+      const audioBlob = new Blob(audioChunks.current, { type: "audio/wav" });//audioChunksに蓄積された録音データをwavに変換
+      const url = URL.createObjectURL(audioBlob);//録音データ(wav)を再生可能なURLに変換↓保存
       setAudioURL(url);
-      audioChunks.current = [];
+      audioChunks.current = [];//蓄積したデータを初期化
     };
-    mediaRecorder.current.start();
-    setRecording(true);
+    mediaRecorder.current.start();//実際にはここで録音開始
+    setRecording(true);//録音状態を有効に変更(ボタンの変化などに用いる)
   };
 
   const stopRecording = () => {
